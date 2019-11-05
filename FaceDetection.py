216fb50090a4f249d565288fbe81b0590daaa48a
kdahan <kerenasaurus@gmail.com>
2019-11-05 13:08:08 -0500

    Fixed face detection

--- FaceDetection.py
+++ FaceDetection.py
@@ -21,20 +21,21 @@ while 1:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Detects faces of different sizes in the input image
-    # faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    face_cascade = cv2.CascadeClassifier("/Users/keren_dahan/PycharmProjects/FaceSwap/venv/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-    # for (x, y, w, h) in faces:
-    #     # To draw a rectangle in a face
-    #     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
-    #     roi_gray = gray[y:y + h, x:x + w]
-    #     roi_color = img[y:y + h, x:x + w]
-    #
-    #     # Detects eyes of different sizes in the input image
-    #     eyes = eye_cascade.detectMultiScale(roi_gray)
-    #
-    #     # To draw a rectangle in eyes
-    #     for (ex, ey, ew, eh) in eyes:
-    #         cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
+    for (x, y, w, h) in faces:
+        # To draw a rectangle in a face
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = img[y:y + h, x:x + w]
+
+        # Detects eyes of different sizes in the input image
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        # To draw a rectangle in eyes
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
 
             # Display an image in a window
     cv2.imshow('img', img)
